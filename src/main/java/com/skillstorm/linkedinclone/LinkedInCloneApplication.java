5e42a950e0e52d2c29bf0c003ffb98e1b450987e
Austin Perrine <akperrine@gmail.com>
2023-09-12 09:05:04 -0700

    initial commit

---
+++ src/main/java/com/skillstorm/linkedinclone/LinkedInCloneApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.linkedinclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedInCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedInCloneApplication.class, args);
+	}
+
+}
